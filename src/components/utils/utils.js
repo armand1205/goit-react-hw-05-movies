5636a40898a15997159d21a1459f40db36878a59
armand1205 <135024222+armand1205@users.noreply.github.com>
2025-03-18 13:15:56 +0200

    commit

---
+++ src/components/utils/utils.js
@@ -0,0 +1,5 @@
+const API_KEY = 'f5b741e843e50a30377824b2ca2057b9';
+const getTrendMovieUrl = `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=f5b741e843e50a30377824b2ca2057b9`;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export { API_KEY, BASE_URL, getTrendMovieUrl };
